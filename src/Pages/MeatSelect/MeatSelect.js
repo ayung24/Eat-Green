27071c1943f0e0adc85d775e667dc7cc1a080aef
michelleteh <nmteh17@students.cs.ubc.ca>
2021-01-10 06:03:45 +0700

    grid commit

--- src/Pages/MeatSelect/MeatSelect.js
+++ src/Pages/MeatSelect/MeatSelect.js
@@ -1,19 +1,77 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+
 
 import * as Constants from '../../Constants/Constants';
 
-class MeatSelect extends Component{
-    render(){
-        return(
-            <div>
-                <p>This is the meat select page</p>
+const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    paper: {
+      padding: theme.spacing(2),
+      textAlign: 'center',
+      color: theme.palette.text.secondary,
+    },
+  }));
+
+
+
+    function MeatSelect() {
+    const classes = useStyles();
+  
+    return (
+      <div className={classes.root}>
+        <Grid container spacing={3}>
+          <Grid item xs={12} container>
+          </Grid>
+          <Grid item xs={12} container>
+          </Grid>
+          <Grid item xs={12} container>
+          </Grid>
+          <Grid item xs={4}>
+          </Grid>
+          <Grid item xs={4}>
+            <Paper className={classes.paper}> Select Your Meat Dish! </Paper>
+          </Grid>
+          <Grid item xs={4}>
+          </Grid>
+          <Grid item xs={3}>
+            <Paper className={classes.paper}>xs=3</Paper>
+          </Grid>
+          <Grid item xs={3}>
+            <Paper className={classes.paper}>xs=3</Paper>
+          </Grid>
+          <Grid item xs={3}>
+            <Paper className={classes.paper}>xs=3</Paper>
+          </Grid>
+          <Grid item xs={3}>
+            <Paper className={classes.paper}>xs=3</Paper>
+          </Grid>
+          <Grid item xs={4}>
+          </Grid>
+          <Grid item xs={4}>
+            <Paper className={classes.paper}> Total Protein </Paper>
+          </Grid>
+          <Grid item xs={4}>
+          </Grid>
+          <Grid item xs={4}>
+          </Grid>
+          <Grid item xs={4}>
+            <Paper className={classes.paper}> Next </Paper>
+          </Grid>
+          <Grid item xs={4}>
+          </Grid>
+        </Grid>
+        <p>This is the meat select page</p>
                 <Link to={Constants.ROUTE_VEG_SELECT}>
                     <button>Click here to continue to veggie selection...</button>
                 </Link>
-            </div>
-        )
-    }
-}
+      </div>
+    );
+  }
 
 export default MeatSelect;
