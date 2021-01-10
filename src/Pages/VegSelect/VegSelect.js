884b8e1841d5dd3a04a37dbca593637915f570e5
Alice <alicekanng@gmail.com>
2021-01-09 17:07:21 -0800

    Made progress bar

--- src/Pages/VegSelect/VegSelect.js
+++ src/Pages/VegSelect/VegSelect.js
@@ -1,7 +1,14 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
+// import {View} from 'react-native';
+import ProgressBar from "../../Components/ProgressBar/ProgressBar";
 
 import * as Constants from '../../Constants/Constants';
+// import {arr} from '../MeatSelect';
+
+const testData = [
+    { bgcolor: "#00695c", completed: 50 }
+  ];
 
 class VegSelect extends Component{
     render(){
@@ -11,6 +18,11 @@ class VegSelect extends Component{
                 <Link to={Constants.ROUTE_SUMMARY}>
                     <button>Click here to continue to summary page...</button>
                 </Link>
+
+                {testData.map((item, idx) => (
+                <ProgressBar key={idx} bgcolor={item.bgcolor} completed={item.completed}>
+                </ProgressBar>
+                ))}
             </div>
         )
     }
