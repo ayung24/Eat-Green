1f2550932c37eadc2b0bc462d678014813655a2a
Alice <alicekanng@gmail.com>
2021-01-10 04:52:49 -0800

    Created summary page without the data

--- src/Pages/Summary/Summary.js
+++ src/Pages/Summary/Summary.js
@@ -3,6 +3,25 @@ import {Link} from 'react-router-dom';
 import { db } from "../../firebase";
 
 import * as Constants from '../../Constants/Constants';
+import Button from '@material-ui/core/Button';
+import HomeIcon from '@material-ui/icons/Home'
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles'
+import { green } from '@material-ui/core/colors';
+import CssBaseline from "@material-ui/core/CssBaseline";
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: green[900]
+        },
+        background: {
+            default: "#AED581"
+        }
+    }
+})
 
 class Summary extends Component{
     constructor(props) {
@@ -50,12 +69,67 @@ class Summary extends Component{
       }
     render(){
         return(
-            <div>
-                <p>This is the summary page</p>
-                <Link to={Constants.ROUTE_HOME}>
-                    <button>Click here to continue to go back to home page...</button>
-                </Link>
+            <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <div style={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                height: '100vh'}}>
+                <Grid>
+                <div style={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    padding: 50}}>
+                    <Typography variant = "h6">
+                        Here is your meal completely in meat alternatives—enjoy!
+                    </Typography>
+                </div>
+
+                <div style={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    height: '30vh'}}>
+                    <Grid container spacing = {5} justify = "center">
+                        <Grid item>
+                            <Paper style = {{ height:300, width:200 }}></Paper>
+                        </Grid>
+                        <Grid item>
+                            <Paper style = {{ height:300, width:200 }}></Paper>
+                        </Grid>
+                    </Grid>
+                </div>
+
+                <div style={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    padding: 50}}>
+                    <Typography variant = "h6">
+                        Your total intake of protein from this meal is:
+                    </Typography>
+                </div>
+
+                <div style={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    height: '10vh'}}>
+                        <Link to={Constants.ROUTE_HOME}>
+                        <Button
+                        endIcon={<HomeIcon />}
+                        size="large"
+                        variant="contained"
+                        color="primary">
+                            BACK TO HOME
+                        </Button>
+                        </Link>
+                </div>
+                </Grid>
             </div>
+            </ThemeProvider>
         )
     }
 }
