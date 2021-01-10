f958d21b8d070b88316b67b97176deba35c7394e
Amy <ayung.024@gmail.com>
2021-01-09 17:18:00 -0800

    add counter
    counter component made

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,5 @@
 import firebase from "firebase";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyAW_zNfGGnRUZ3qp4L2TMHavg14PJ8m7Fk",
-    authDomain: "coastal-haven-196323.firebaseapp.com",
-    databaseURL: "https://coastal-haven-196323-default-rtdb.firebaseio.com",
-    projectId: "coastal-haven-196323",
-    storageBucket: "coastal-haven-196323.appspot.com",
-    messagingSenderId: "494124905574",
-    appId: "1:494124905574:web:5503480450ddda6fbde9a7",
-    measurementId: "G-65WR5HK210"
-};
-
 const firebaseApp = firebase.initializeApp({
     apiKey: "AIzaSyAW_zNfGGnRUZ3qp4L2TMHavg14PJ8m7Fk",
     authDomain: "coastal-haven-196323.firebaseapp.com",
